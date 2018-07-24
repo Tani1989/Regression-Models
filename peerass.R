cb0ad31c5d399bd61027ab4dfc5c7287acab6825
tanika1989 <tanika.sh@gmail.com>
2018-07-24 16:47:06 -0500

    ASSIGN

--- peerass.R
+++ peerass.R
@@ -12,3 +12,25 @@ data("mtcars")
 head(mtcars)
 names(mtcars)
 summary(mtcars)
+
+# Start Analyzing the data
+# We need to see the relationship between mpg and other variables.
+
+cor(mtcars$mpg,mtcars[,-1])
+
+# As we can see that there is negative relation between mpg and cyl,disp,hp,wt,carb.
+
+# 1. Is an automatic or manual transmission better for MPG.
+
+?mtcars
+
+Transmission(0 = automatic, 1 = manual)
+
+# some variables need to be converted into factor?
+str(mtcars)
+mtcars$cyl <- factor(mtcars$cyl)
+mtcars$vs <- factor(mtcars$vs)
+mtcars$am <- factor(mtcars$am)
+mtcars$gear <- factor(mtcars$gear)
+mtcars$carb <- factor(mtcars$carb)
+
